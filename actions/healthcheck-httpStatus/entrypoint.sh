65bf7731c0649ffd410fa658a14be507d200979a
pinkal vansia <p.vansia@gmail.com>
2021-10-08 14:06:33 +0530

    fixed if condition

--- actions/healthcheck-httpStatus/entrypoint.sh
+++ actions/healthcheck-httpStatus/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -l
 
+echo "curl -o /dev/null -s -w '%{http_code}\n' --user $2 $1"
 http_code=$(curl -o /dev/null -s -w "%{http_code}\n" --user $2 $1)
 
 echo $http_code
-if [ $ret -ne 0 ]; then
+if [ $? -ne 0 ]; then
     exit 1
 fi
