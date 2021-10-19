5ac6a1961187460f34a055d1b3d4d0e871390ee0
cn-devflows-prod <devflows-operations@devfactory.com>
2021-10-19 09:31:08 +0100

    Support list of s3 buckets on the healthcheck

--- actions/healthcheck-s3bucket/README.md
+++ actions/healthcheck-s3bucket/README.md
@@ -1,13 +1,13 @@
 # URL health check action
 
-A simple upload to an s3 bucket as healthcheck
+A simple upload to s3 buckets as a healthcheck
 
 ```yaml
 steps:
   - name: s3 healthcheck
     uses: trilogy-group/.github/actions/healthcheck-s3bucket@actions
     with:
-      bucket-name: samplebucket
+      bucket-name: samplebucket|samplebucket2
 ```
 
 The action will fail if an upload cannot be performed on the bucket. Make sure the AWS credentials have appropiate permissions and the PutObject action
--- actions/healthcheck-s3bucket/index.js
+++ actions/healthcheck-s3bucket/index.js
@@ -2,29 +2,35 @@ import * as core from "@actions/core";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { s3Client } from "./libs/s3Client.js"; // Helper function that creates Amazon S3 service client module.
 
-// Set the parameters.
-const bucketParams = {
-  Bucket: core.getInput("bucket-name"),
-  Key: "healthcheck.txt",
-  // Content of the new object.
-  Body: "Healtcheck successful. This file can be deleted",
-};
+
+
+const s3BucketString = core.getInput("bucket-names", { required: true });
+const s3Buckets = urlString.split("|");
 
 // Create and upload the object to the specified Amazon S3 bucket.
 const run = async () => {
   try {
-    const data = await s3Client.send(new PutObjectCommand(bucketParams));
-    core.info("Success");
-    console.log(
-      "Successfully uploaded object: " +
-        bucketParams.Bucket +
-        "/" +
-        bucketParams.Key
-    );
-    return data;
+    for (const s3Bucket of s3Buckets) {
+      // Set the parameters.
+      const bucketParams = {
+        Bucket: s3Bucket,
+        Key: "healthcheck.txt",
+        // Content of the new object.
+        Body: "Healtcheck successful. This file can be deleted",
+      };
+      const data = await s3Client.send(new PutObjectCommand(bucketParams));
+      core.info(bucketParams.Bucket + " Success");
+      console.log(
+        "Successfully uploaded object: " +
+          bucketParams.Bucket +
+          "/" +
+          bucketParams.Key
+      );
+    // return data;
+    }
   } catch (err) {
     console.log("Error", err);
-    core.setFailed(e.message);
+    core.setFailed(e.message + "\n Failed to upload file to: " + bucketParams.Bucket);
   }
 };
 run();
